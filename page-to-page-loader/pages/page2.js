9b2ad811079539176154fb2443f31d53279d7b70
Ahmed Tahir <70894942+ahmedtahir2311@users.noreply.github.com>
2023-02-03 21:01:34 +0500

    Page to page loader

---
+++ page-to-page-loader/pages/page2.js
@@ -0,0 +1,13 @@
+
+
+export default function page2({}) {
+    console.log();
+    return (
+        <div>
+        <h1>This is the SSR page</h1>
+        <p>This is the SSR page</p>
+        </div>
+    )
+}
+
+
